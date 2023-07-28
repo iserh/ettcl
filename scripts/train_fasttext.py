a5c6d8fd0e7aadb5e0faef3342cb823a9b3287ae
Henri Iser <henri.iser@iais.fraunhofer.de>
2023-07-28 21:52:36 +0200

    FastText

--- scripts/train_fasttext.py
+++ scripts/train_fasttext.py
@@ -1,41 +1,40 @@
 from pathlib import Path
 
-from datasets import Dataset, load_dataset
+from datasets import load_from_disk, Split
 from gensim.models import FastText
 from gensim.utils import simple_preprocess
 
 from ettcl.logging import configure_logger
+import os
 
 configure_logger("INFO")
 
-dataset_name = "imdb"
+dataset_path = "/home/IAIS/hiser/data/imdb"
+dataset_name = os.path.basename(dataset_path)
 text_column = "text"
 label_column = "label"
 
-train_dataset = load_dataset(dataset_name, split="train")
-train_dataset = train_dataset.rename_columns({text_column: "text", label_column: "label"})
+dataset = load_from_disk(dataset_path)[Split.TRAIN]
 
 
 def preprocess(text: str, max_length: int = 512, **kwargs) -> list[str]:
     return {"tokens": simple_preprocess(text, **kwargs)[:max_length]}
 
 
-corpus = Dataset.from_dict({"text": train_dataset["text"]})
-
-corpus = corpus.map(
+dataset = dataset.map(
     preprocess,
     input_columns="text",
     num_proc=8,
 )
 
-model = FastText(vector_size=128, window=5, min_count=5)
-model.build_vocab(corpus_iterable=corpus["tokens"])
+model = FastText(vector_size=128, window=5, min_count=5, word_ngrams=1)
+model.build_vocab(corpus_iterable=dataset["tokens"])
 total_words = model.corpus_total_words
 total_examples = model.corpus_count
 print(f"{total_examples=}")
 print(f"{total_words=}")
 
-model.train(corpus_iterable=corpus["tokens"], total_words=total_words, total_examples=total_examples, epochs=10)
+model.train(corpus_iterable=dataset["tokens"], total_words=total_words, total_examples=total_examples, epochs=10)
 
-Path("models/imdb").mkdir(exist_ok=True, parents=True)
-model.save("models/imdb/fasttext")
+Path(f"models/{dataset_name}").mkdir(exist_ok=True, parents=True)
+model.save(f"models/{dataset_name}/fasttext")
